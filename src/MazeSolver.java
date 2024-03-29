8ccfc167cc6018b4655a092407211abd339f5712
Beckett Knuff <beckett.knuff@menloschool.org>
2024-03-28 23:19:37 -0700

    Check cells is dealt with, for now. Plus the winning path stack code.

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -5,6 +5,8 @@
  */
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class MazeSolver {
@@ -33,7 +35,7 @@ public class MazeSolver {
         // Remember to reverse the stack in order
         Stack<MazeCell> stack = new Stack<>();
         // The actual path in the maze
-        ArrayList<MazeCell> path = new ArrayList<>();
+        ArrayList<MazeCell> solution = new ArrayList<>();
         MazeCell current = this.maze.getEndCell();
         while (current != null) {
             // push each cell on the stack
@@ -43,9 +45,9 @@ public class MazeSolver {
         }
         // Now pop each cell I added to the stack to reverse the order (add to the winning path)
         while (!stack.isEmpty()) {
-            path.add(stack.pop());
+            solution.add(stack.pop());
         }
-        return path;
+        return solution;
     }
 
     /**
@@ -55,7 +57,35 @@ public class MazeSolver {
     public ArrayList<MazeCell> solveMazeDFS() {
         // TODO: Use DFS to solve the maze
         // Explore the cells in the order: NORTH, EAST, SOUTH, WEST
-        return null;
+        Stack<MazeCell> stack = new Stack<>();
+        ArrayList<MazeCell> solution = new ArrayList<>();
+        stack.push(this.maze.getStartCell());
+        // while the stack has some cells in it, identify if it has been explored
+        while (!stack.isEmpty()) {
+            MazeCell current = stack.pop();
+            if (!current.isExplored()) {
+                current.setExplored(true);
+                if (current == this.maze.getEndCell()) {
+                    return this.getSolution();
+                }
+
+                // never eat soggy waffles
+                int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+                // for each direction find the value to see if the cell or direction is a valid path/option
+                for (int[] c : directions) {
+                    int newRow = current.getRow() + c[0];
+                    int newCol = current.getCol() + c[0];
+                    if (this.maze.isValidCell(newRow, newCol)) {
+                        MazeCell next = this.maze.getCell(newRow, newCol);
+                        // set the new parent cell to the now MazeCell
+                        next.setParent(current);
+                        // if so, push new cell onto stack
+                        stack.push(next);
+                    }
+                }
+            }
+        }
+        return solution; // if no path
     }
 
     /**
@@ -65,7 +95,31 @@ public class MazeSolver {
     public ArrayList<MazeCell> solveMazeBFS() {
         // TODO: Use BFS to solve the maze
         // Explore the cells in the order: NORTH, EAST, SOUTH, WEST
-        return null;
+        Queue<MazeCell> queue = new LinkedList<>();
+        queue.add(this.maze.getStartCell());
+
+        while (!queue.isEmpty()) {
+            MazeCell current = queue.poll();
+            if (!current.isExplored()) {
+                current.setExplored(true);
+                if (current == this.maze.getEndCell()) {
+                    return this.getSolution();
+                }
+
+                // similar to solving DFS just with queue so add each new cell as presented (first in first out)
+                int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+                for (int[] c : directions) {
+                    int newRow = current.getRow() + c[0];
+                    int newCol = current.getCol() + c[0];
+                    if (this.maze.isValidCell(newRow, newCol)) {
+                        MazeCell next = this.maze.getCell(newRow, newCol);
+                        next.setParent(current);
+                        queue.add(next);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(); // if no path works
     }
 
     public static void main(String[] args) {
